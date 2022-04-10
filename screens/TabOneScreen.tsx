301efac8ab11e9ea45d0fe4a003fe5883afa244f
mamieorine <mamieorine@gmail.com>
2022-04-10 14:33:51 +0100

    Fix current location

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -16,7 +16,7 @@ export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Current Location</Text>
-      <Text style={styles.getStartedText}>Still not working</Text>
+      {/* <Text style={styles.getStartedText}>Still not working</Text> */}
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 
       {/* <GooglePlacesInput /> */}
