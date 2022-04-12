411b7be6b7eb0586c6b590463e1622ad12c02502
mamieorine <mamieorine@gmail.com>
2022-04-12 12:48:33 +0100

    POC autoComplete

--- components/AddressSearch.tsx
+++ components/AddressSearch.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 const GooglePlacesInput = () => {
@@ -5,9 +6,24 @@ const GooglePlacesInput = () => {
 		<GooglePlacesAutocomplete
 			placeholder='Search destination'
 			minLength={2}
+			onFail={error => console.log('error' + error)}
 			onPress={(data, details = null) => {
 				// 'details' is provided when fetchDetails = true
 				console.log(data, details);
+			}}
+			styles={{
+				textInputContainer: {
+				  backgroundColor: 'grey',
+				},
+				textInput: {
+				  height: 38,
+				  color: '#5d5d5d',
+				  fontSize: 16,
+				  width: 200
+				},
+				predefinedPlacesDescription: {
+				  color: '#1faadb',
+				},
 			}}
 			query={{
 				key: 'AIzaSyDyqPFPoJGT53p6-QosVbvV16MUwIL38Uo',
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native';
-
 import { Text, View } from '../components/Themed';
 import GooglePlacesInput from '../components/AddressSearch';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
@@ -18,8 +17,7 @@ export default function TabTwoScreen() {
       <Text style={styles.title}>Current Location</Text>
       {/* <Text style={styles.getStartedText}>Still not working</Text> */}
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-
-      {/* <GooglePlacesInput /> */}
+      <GooglePlacesInput />
       <MapView region={region} style={{height: '50%', width: '100%'}}
         showsUserLocation={true}
         provider={PROVIDER_GOOGLE}
