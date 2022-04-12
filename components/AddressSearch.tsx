ee4786d40873a1c0e9e3c359adce848029050007
mamieorine <mamieorine@gmail.com>
2022-04-12 22:46:36 +0100

    Return lat and lng when click place

--- components/AddressSearch.tsx
+++ components/AddressSearch.tsx
@@ -1,28 +1,64 @@
 import React from 'react';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { useState } from 'react';
 
-const GooglePlacesInput = () => {
+export type Geometry = {
+	latitude: Number;
+	longitude: Number
+}
+
+const GooglePlacesInput = (props: { geometry: Geometry; onDestinationChange: any; }) => {
+	const [geometry, onGeometryChange] = useState({
+		latitude: 51.2419894,
+		longitude: -0.7035547
+	});
+
+	props.onDestinationChange(geometry);
+
   return (
 		<GooglePlacesAutocomplete
 			placeholder='Search destination'
 			minLength={2}
 			onFail={error => console.log('error' + error)}
+			fetchDetails={true}
+			currentLocation={true}
 			onPress={(data, details = null) => {
-				// 'details' is provided when fetchDetails = true
-				console.log(data, details);
+				if (!details) return;
+
+				onGeometryChange({
+					latitude: details.geometry.location.lat,
+					longitude: details.geometry.location.lng
+				})
 			}}
 			styles={{
+				container: {
+					zIndex: 10,
+					paddingBottom: 0,
+					flexGrow: 1
+				},
 				textInputContainer: {
 				  backgroundColor: 'grey',
+				  width: '100%',
+				  padding: 10,
 				},
 				textInput: {
-				  height: 38,
+				  height: 45,
 				  color: '#5d5d5d',
 				  fontSize: 16,
-				  width: 200
 				},
 				predefinedPlacesDescription: {
 				  color: '#1faadb',
+				},
+				listView: {
+					position: 'absolute',
+					top: 60,
+					left: 10,
+					right: 10,
+					backgroundColor: 'white',
+					borderRadius: 5,
+					flex: 1,
+					elevation: 3,
+					zIndex: 10
 				},
 			}}
 			query={{
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,27 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 import { Text, View } from '../components/Themed';
 import GooglePlacesInput from '../components/AddressSearch';
+import MapViewDirections from 'react-native-maps-directions';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const origin = {latitude: 51.2419894, longitude: -0.7035547};
+const destination = {latitude: 51.2419894, longitude: -1.7135547};
+const GOOGLE_MAPS_APIKEY = 'AIzaSyDyqPFPoJGT53p6-QosVbvV16MUwIL38Uo';
 
 export default function TabTwoScreen() {
-  const [region, onRegionChange] = useState({
-    latitude: 51.2419894,
-    longitude: -0.7035547,
-    latitudeDelta: 1,
-    longitudeDelta: 2.5,
-  });
+  const [geometry, onGeometryChange] = useState({
+		latitude: 51.2419894,
+		longitude: -0.7035547
+	});
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Current Location</Text>
-      {/* <Text style={styles.getStartedText}>Still not working</Text> */}
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <GooglePlacesInput />
-      <MapView region={region} style={{height: '50%', width: '100%'}}
+      <GooglePlacesInput geometry={geometry} onDestinationChange={onGeometryChange} />
+      <MapView region={{... geometry, latitudeDelta: 1, longitudeDelta: 2.5}} style={{margin: 0, height: 400, width: '100%'}}
         showsUserLocation={true}
-        provider={PROVIDER_GOOGLE}
-      />
+        provider={PROVIDER_GOOGLE} >
+
+        {/* <View>
+          <MapViewDirections
+            origin={origin}
+            destination={destination}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={4}
+            strokeColor="hotpink"
+            mode="DRIVING"
+          />
+        </View> */}
+      </MapView>
+
+
+      <Text style={styles.title}>{geometry.latitude}</Text>
     </View>
   );
 }
@@ -30,10 +46,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   title: {
     fontSize: 20,
+    marginTop: 20,
     fontWeight: 'bold',
   },
   getStartedText: {
